864372682798d8fc4e4f4e3bb45b2f3616713327
Muhammad Ferdinansyah Arighi <ferdinansyarighi@gmail.com>
2021-11-01 13:36:29 +0700

    push project

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard_v');
 });
+
+Route::get('dashboard', 'App\Http\Controllers\Dashboard_c@index')->name('dashboard');
